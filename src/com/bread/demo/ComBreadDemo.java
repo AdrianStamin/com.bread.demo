d293d683fc9ac7dcf6f5d25f08f3017b4fbe9969
adi <adrian.stamin@yahoo.com>
2013-09-30 16:30:13 +0300

    small ingredient change

--- src/com/bread/demo/ComBreadDemo.java
+++ src/com/bread/demo/ComBreadDemo.java
@@ -14,6 +14,6 @@ public class ComBreadDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Some ingredient code");
     }
 }
