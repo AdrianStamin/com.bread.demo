b4d5cf9508caa1e952680bb2301ef85b7c31857e
adi <adrian.stamin@yahoo.com>
2013-09-30 16:37:38 +0300

    added sezonalBread

---
+++ src/com/bread/demo/SezonalBread.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bread.demo;
+
+/**
+ *
+ * @author Adi
+ */
+public class SezonalBread {
+    private String sezonal;
+    
+    public SezonalBread(){
+      this.sezonal="sezonal";
+    }
+}
